ec23a8717b9b470a35b41588e6b73d20f2550a8c
tom803 <tom803@users.noreply.github.com>
2021-10-20 11:36:13 +0200

    update sndBugfix, changes on square, circle

--- src/Circle.java
+++ src/Circle.java
@@ -1,6 +1,6 @@
 public class Circle {
 	@Override
 	public String toString() {
-		return "I'm a circle.";
+		return "I'm a circle. ... Am i an ellipse?";
 	}
 }
--- src/Square.java
+++ src/Square.java
@@ -1,6 +1,6 @@
 public class Square {
 	@Override
 	public String toString() {
-		return "I'm a square.";
+		return "I'm a square. ... Am i an rectangle?";
 	}
 }
