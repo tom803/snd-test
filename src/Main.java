786ed19f873321e301954851fcb7f5435d658b96
tom803 <tom803@users.noreply.github.com>
2021-10-20 12:02:05 +0200

    standardised Square.java, sorted output in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello world.");
 		System.out.println(new Rectangle());
+		System.out.println(new Square());
+		System.out.println(new Ellipse());
 		System.out.println(new Circle());
 		System.out.println(new Triangle());
-		System.out.println(new Ellipse());
-		System.out.println(new Square());
 	}
 }
